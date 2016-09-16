a66a7c1fd684c675cddb3da8125028eb33aa4b67
Moisés Cachay Tello <mcachay@phantasia.pe>
2016-09-15 22:25:43 -0500

    Initial commit

---
+++ deck.rb
@@ -0,0 +1,14 @@
+require 'squib'
+
+data = Squib.csv file: 'data.csv'
+
+Squib::Deck.new(cards: data['name'].size, layout: 'layout.yml', width: '8.2cm', height: '8.2cm') do
+  rect layout: 'bleed'
+  rect layout: 'border'
+  rect layout: 'descriptionbox'
+
+  text str: data['name'], layout: 'title'
+  # text str: data['team']
+  text str: data['description']
+  save_png trim: 10
+end
